252e9b35fe694101a39cd3e80235cbecabc1bdfa
LahiruHarshana <lharshana2002@gmail.com>
2023-11-01 22:47:25 +0530

    updated

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 operator = input("Enter an operator(+ - / *)")
 num1 = float(input("Enter 1st Number : "))
 num2 = float(input("Enter 2nd Number : "))
+
+if operator == "+":
+    result = num1+num2
+elif operator =="-"
